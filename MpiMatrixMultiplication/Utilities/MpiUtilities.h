eb1e6318fd9ac81cbff8357a16e6f1a6121801a5
Valentin Begunov <bvival@gmail.com>
2020-02-17 12:19:22 +1000

    Added MpiUtilities;

---
+++ MpiMatrixMultiplication/Utilities/MpiUtilities.h
@@ -0,0 +1,11 @@
+#ifndef MPIUTILITIES_H
+#define MPIUTILITIES_H
+#include <string>
+#include "mpi.h"
+#include <iostream>
+
+using namespace std;
+
+void MpiErrorExit(string msg);
+
+#endif // MPIUTILITIES_H
