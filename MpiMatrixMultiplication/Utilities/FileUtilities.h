b601c2955856bd8547de80bb2249c1b730737fea
Valentin Begunov <bvival@gmail.com>
2020-02-17 12:18:50 +1000

    Added FileUtilities;

---
+++ MpiMatrixMultiplication/Utilities/FileUtilities.h
@@ -0,0 +1,22 @@
+#ifndef _FILEUTILITIES_
+#define _FILEUTILITIES_
+#include <string>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+#include "StringUtilities.h"
+
+
+
+using namespace std;
+
+bool FileExists(const string& filename);
+bool IsFileClosedSuccessfully(ofstream* file);
+ofstream* CreateNewFile(const string& path, const string& name, bool reCreateIfAlreadyExists);
+bool IsFileOpen(ofstream* file);
+bool TryWriteToFile(ofstream* file, const vector<uint32_t>& values);
+
+string GetFilePath(const string& fileName, string subFolder);
+ifstream GetReadStream(const string& filePath);
+
+#endif
