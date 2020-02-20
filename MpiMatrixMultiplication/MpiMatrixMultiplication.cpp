7c1addce65ede9e5c1b02bda0794d2829e216a5d
Valentin Begunov <bvival@gmail.com>
2020-02-20 17:04:16 +1000

    Work continues;

--- MpiMatrixMultiplication/MpiMatrixMultiplication.cpp
+++ MpiMatrixMultiplication/MpiMatrixMultiplication.cpp
@@ -1,15 +1,15 @@
-﻿//#include <mpi.h>
-//#include "Utilities/MatrixUtilities.h"
-//#include "Utilities/InputUtilities.h"
-//
+﻿
 //#define BUFFER_SIZE 1024
 #define FILE_NAME1 "EntryMatrixDouble1.dat"
 #define FILE_NAME2 "EntryMatrixDouble2.dat"
 #define RESULT_FILE_PATH "C:\\Users\\Public\\Pictures"
 #define RESULT_FILE_NAME "MpiTest.txt"
-//
-//using namespace std;
-//
+//double start_time, end_time;
+//start_time = MPI_Wtime();
+//Think about using MPI_Barrier();
+//Use MPI_Gather();
+//Use MPI_type_vector() & MPI_commit()
+
 //MPI_Status status;
 //
 //#define N 10
@@ -24,8 +24,8 @@
 //	SYSTEMTIME start, stop;
 //
 //	//getchar();
-//	MatrixClass matrix1 = FromFile(FILE_NAME1);
-//	MatrixClass matrix2 = FromFile(FILE_NAME2);
+//	MatrixClass matrix1 = FromFile(FILE_NAME1, "\\Data\\");
+//	MatrixClass matrix2 = FromFile(FILE_NAME2, "\\Data\\");
 //	//ToDo: optimize matrix2 & transposedMatrix
 //	MatrixClass transposedMatrix = Transpose(matrix2);
 //
@@ -137,17 +137,7 @@
 //
 //}
 //
-//MatrixClass Transpose(MatrixClass& data) {
-//	vector<vector<double>> result(data.GetI(), vector<double>(data.GetJ()));
-//
-//	for (int a = 0; a < data.GetI(); a++)
-//		for (int b = 0; b < data.GetJ(); b++) {
-//			result[a][b] = data.Get(b, a);
-//		}
-//
-//	return MatrixClass(data.GetJ(), data.GetI(), result);
-//
-//}
+
 
 #include <iostream>
 #include "mpi.h"
@@ -167,21 +157,49 @@ int main(int argc, char** argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskId);
 	auto vectorSize = 5;
-	
-	if (taskId == 0)
+
+	MatrixClass matrix1 = FromApplicationFolderFile(FILE_NAME1, "\\Data\\");
+	MatrixClass matrix2 = FromApplicationFolderFile(FILE_NAME2, "\\Data\\");
+	//ToDo: checking on 2x3 & 3x2 - only calculate 500x500 or 2x2 (square matrices)
+	/*if (taskId == 0)
 	{
-		
 		auto file = CreateNewFile(RESULT_FILE_PATH, RESULT_FILE_NAME, true);
-		if(!TryWriteToFile(file, GetVectorById(taskId, vectorSize, size)))
+		if (!TryWriteToFile(file, GetVectorById(taskId, vectorSize, size)))	MpiErrorExit("TryWriteToFile: return false");
+
+		if (!IsFileClosedSuccessfully(file)) MpiErrorExit("IsFileClosedSuccessfully: return false");
+
+	}*/
+	MPI_Status status1;
+	try
+	{
+		int request;
+		int с[10] = { 1,0,0,0,0,0,0,0,0,1 };
+		int jk[10];
+		if (taskId == 0)
 		{
-			MpiErrorExit("TryWriteToFile: return false");
+			request = MPI_Send(&с[0], 10, MPI_INT, 1, 99, MPI_COMM_WORLD);
+			cout << "Request = ";
+			cout << request;
 		}
-		if (!IsFileClosedSuccessfully(file)) {
-			MpiErrorExit("IsFileClosedSuccessfully: return false");
+		else if (taskId == 1)
+		{
+			MPI_Recv(&jk[0], 10, MPI_INT, 0, 99, MPI_COMM_WORLD, &status1);
+			ofstream File(R"(C:\Users\Public\Pictures\TestReceive)");
+			if (File.is_open())
+			{
+				for (int element : jk)
+				{
+					File << element << endl;
+				}
+				File.close();
+			}
 		}
 	}
+	catch (const char* msg)
+	{
+		cout << msg;
+	}
 
-	
 	cout << " Number of process = " << size << "   My rank = " << taskId << endl;
 	MPI_Finalize();
 	system("pause");
